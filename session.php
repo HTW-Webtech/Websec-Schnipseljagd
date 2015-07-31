3dd30260e2ce08953bd541ba2b88f2d8dd274370
Max Beier <beierm@htw-berlin.de>
2015-07-31 19:33:55 +0200

    Standard-Zeitzone gesetzt

--- session.php
+++ session.php
@@ -1,5 +1,7 @@
 <?php
 
+   date_default_timezone_set('Europe/Berlin');
+
    ini_set('session.cookie_lifetime', 7 * 24 * 60 * 60);
    ini_set('session.gc_maxlifetime', 7 * 24 * 60 * 60);
    ini_set('session.save_path', dirname(__FILE__) . '/assets/sessions');
